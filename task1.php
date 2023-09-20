3dc8478164503de29bc312616224b106ce186bb9
Habibur <habibur500257@gmail.com>
2023-09-20 23:12:13 +0600

    Task 4: Fibonacci Series printing using a Function

--- task1.php
+++ task1.php
@@ -90,3 +90,21 @@ for ($i = 0; $i < $limit; $i++) {
     $b = $next;
 }
 
+//Task 4: Fibonacci Series printing using a Function
+echo "\n\n=============Task 4=============\n";
+
+function printFibonacciSeries($count) {
+    $a = 0;
+    $b = 1;
+
+    for ($i = 0; $i < $count; $i++) {
+        echo $a . " ";
+
+        $next = $a + $b;
+
+        $a = $b;
+        $b = $next;
+    }
+}
+
+printFibonacciSeries(15);
