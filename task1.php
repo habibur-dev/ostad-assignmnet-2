847cdefcbb9fdb184dabde7a9a9c7083168c474a
Habibur <habibur500257@gmail.com>
2023-09-20 23:04:34 +0600

    Task 2: Skip Multiples of 5

--- task1.php
+++ task1.php
@@ -54,3 +54,18 @@ function evenNumbersDoWhileLoop($start, $end, $step) {
 evenNumbersDoWhileLoop(1, 20, 2);
 
 
+//Task 2: Skip Multiples of 5
+echo "\n\n=============Task 2=============\n";
+function skipFiveMultiple($start, $end, $skip){
+    for($i = $start; $i <= $end; $i++){
+
+        if($i % $skip == 0){
+            continue;
+        }
+
+        echo $i." ";
+    }
+}
+
+skipFiveMultiple(1, 50, 5);
+echo "\n";
